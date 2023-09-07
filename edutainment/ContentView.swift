9a41d0502ec33d4a799f5935d214375506a3ba9d
Mweh <fahmianton@gmail.com>
2023-09-07 19:44:19 +0700

    Initial Commit

---
+++ edutainment/ContentView.swift
@@ -0,0 +1,130 @@
+//
+//  ContentView.swift
+//  edutainment
+//
+//  Created by Muhammad Fahmi on 06/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var multiTable = 2
+    @State private var questionDefaultAmount = 15
+    var questionAmount = [5, 10, 15]
+    @State private var answer = [String](repeating: "", count: 15)
+    @State private var randomNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].shuffled()
+    //    @FocusState private var inputIsFocus: Bool
+    @State private var isShowSetting = true
+    @State private var isStart = false
+    @State private var isCorrect = false
+    @State private var inCorrectAnswer = 0
+    @State private var isShowResult = false
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                if isShowSetting{
+                    Section{
+                        Stepper("Multiplication table: \(multiTable)", value: $multiTable, in: 2...12)
+                    } header: {
+                        Text("Select multiplication table").bold()
+                    }
+                    Section{
+                        Picker("", selection: $questionDefaultAmount){
+                            ForEach(questionAmount, id: \.self){
+                                Text("\($0)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    } header: {
+                        Text("How many question").bold()
+                    }
+                }
+                Button(isShowSetting ? "Start" : "Answer \(questionDefaultAmount) question below"){
+                    withAnimation(.default){
+                        isShowSetting = false
+                        isStart = true
+                    }
+                }
+                if isStart{
+                    Section{
+                        VStack(){
+                            ForEach(0..<questionDefaultAmount, id: \.self){ num in
+                                HStack(){
+                                    Text("\(multiTable) x \(num>10 ? randomNumber[num-5] : randomNumber[num]) = ")
+                                        .frame(width: 70, alignment: .center)
+                                    TextField("Answer here", text: $answer[num])
+                                        .foregroundStyle(
+                                            (Int(answer[num]) ?? 0) == (multiTable * (num > 10 ? randomNumber[num - 5] : randomNumber[num])) ? .green : .red)
+                                        .frame(width: 200, alignment: .center)
+                                        .keyboardType(.numberPad)
+                                    //                                            .focused($inputIsFocus)
+                                }
+                            }
+                        }
+                    } header: {
+                        Text("Question").bold()
+                    }
+                    Button("Check answer") {
+                        checkAnswer()
+                        isShowResult.toggle()
+                    }
+                    if isShowResult {
+                        if isCorrect {
+                            Text("All correct!")
+                                .foregroundColor(.green)
+                        } else {
+                            Text("Some answers are incorrect. Try again.")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+//                Button("Debug"){
+//                    print("questionAmount: \(questionAmount)")
+//                    print("questionDefaultAmount: \(questionDefaultAmount)")
+//                }
+            }
+            .navigationTitle("Edutainment")
+                        .toolbar(){
+            //                ToolbarItem(placement: .keyboard){
+            //                    Button("Done"){
+            //                        inputIsFocus = false
+            //                    }
+            //                }
+                            Button("Reset", action: reset)
+                        }
+        }
+    }
+    func reset() {
+        withAnimation(.default){
+            randomNumber.shuffle()
+            isShowSetting = true
+            isStart = false
+            isShowResult = false
+            multiTable = 2
+            questionDefaultAmount = 15
+            answer = [String](repeating: "", count: questionDefaultAmount) // Clear all text entries
+        }
+    }
+    func checkAnswer() {
+        var correctCount = 0
+        
+        for num in 0..<questionDefaultAmount {
+            let userAnswer = Int(answer[num]) ?? 0
+            let correctAnswer = multiTable * (num > 10 ? randomNumber[num - 5] : randomNumber[num])
+            
+            if userAnswer == correctAnswer {
+                correctCount += 1
+            } else {
+                inCorrectAnswer += 1
+            }
+        }
+        isCorrect = correctCount == questionDefaultAmount
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Muhammad Fahmi on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
